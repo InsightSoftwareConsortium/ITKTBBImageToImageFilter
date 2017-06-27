a59908d51a7ee8c342b699b5f9b8acfdf7324f07
Debroize René-paul <rene-paul.debroize@childrens.harvard.edu>
2017-06-27 17:56:02 -0400

    Update TBBImageToImageFilter.remote.cmake

--- TBBImageToImageFilter.remote.cmake
+++ TBBImageToImageFilter.remote.cmake
@@ -1,6 +1,7 @@
 itk_fetch_module(TBBImageToImageFilter
-  "A class derivating imageToImageFilter to optimize thread managment using the TBB library"
+  "This module contains a class inheriting from `itk::ImageToimageFilter` that aim to optimize thread managemet using the TBB library.
+   Insight Journal artical: http://www.insight-journal.org/browse/publication/974"
   GIT_REPOSITORY ${git_protocol}://github.com/rdebroiz/TBBImageToImageFilter.git
   # April 7, 2015 Fixed KWStyle issues
-  GIT_TAG commit sha1 here
-  )
+  GIT_TAG **sha1_here**
+  ) 
